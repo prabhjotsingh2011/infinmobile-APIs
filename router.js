d3d07d7b92e4b9cd87f84585e74b4a4c08841077
Prabhjot Singh <psingh20112002@gmail.com>
2022-03-18 13:50:45 +0530

    signup and login api

--- router.js
+++ router.js
@@ -2,6 +2,6 @@ const router=require('express').Router();
 const authController=require('./Controllers/auth/authController');
 
 router.post('/signup',authController.signUp)
-router.get('/login',authController.login)
+// router.get('/login',authController.login)
 
 module.exports=router;
